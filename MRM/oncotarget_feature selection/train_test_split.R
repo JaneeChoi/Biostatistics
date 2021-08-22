45933d1cb6b2a5a47ebcf78153ce022fd05961db
JaneeChoi <serimtech07@snu.ac.kr>
2021-08-22 22:56:49 +0900

    marker selection using SHAP value

--- MRM/oncotarget_feature selection/train_test_split.R
+++ MRM/oncotarget_feature selection/train_test_split.R
@@ -1,3 +1,5 @@
+#data preprocessing
+
 rawdata<-read.csv("rawdata.csv")
 set.seed(210820)
 library(dplyr)
@@ -23,6 +25,29 @@ training=rbind(training,rawdata[rawdata$type=="NL" | rawdata$type=="OB",][NLSNU_
 test1=rbind(test1,rawdata[ rawdata$type=="NL" | rawdata$type=="OB",][-NLSNU_ind,])
 
 write.csv(training,"train.csv")
+write.csv(test1,"test1.csv")
+write.csv(test2,"test2.csv")
+write.csv(test3,"test3.csv")
+write.csv(test4,"test4.csv")
+
+#order by 68 peptides
+
+train<-read.csv("train.csv")[,-1]
+test1<-read.csv("test1.csv")[,-1]
+test2<-read.csv("test2.csv")[,-1]
+test3<-read.csv("test3.csv")[,-1]
+test4<-read.csv("test4.csv")[,-1]
+
+peptides<-read.csv("68_peptides.csv")[,2]
+gene<-read.csv("68_peptides.csv")[,3]
+
+train<-cbind(train[,1:4],train[,peptides])
+test1<-cbind(test1[,1:4],test1[,peptides])
+test2<-cbind(test1[,1:4],test1[,peptides])
+test3<-cbind(test1[,1:4],test1[,peptides])
+test4<-cbind(test1[,1:4],test1[,peptides])
+
+write.csv(train,"train.csv")
 write.csv(test1,"test1.csv")
 write.csv(test2,"test2.csv")
 write.csv(test3,"test3.csv")
